ec2d4505f4360cbfd9505c242152b6c1221b4ce5
AroSaneD <mail.junk.3135@gmail.com>
2015-02-21 11:35:35 +0200

    lel

---
+++ CustomModellViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomModellViewController.swift
+//  Blower
+//
+//  Created by Arūnas Seniucas on 2/21/15.
+//  Copyright (c) 2015 Arūnas Seniucas. All rights reserved.
+//
+
+import Foundation
---
+++ CustomTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewController.swift
+//  Blower
+//
+//  Created by Arūnas Seniucas on 2/21/15.
+//  Copyright (c) 2015 Arūnas Seniucas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableViewController : UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
