0993b14fca3f1e0028ca4093c12ff7ff5a32a155
Cnnamon <taututis@yahoo.com>
2015-02-21 11:45:45 +0200

    Added facts controller and model

---
+++ FactController.swift
@@ -0,0 +1,18 @@
+//
+//  FactController.swift
+//  Blower
+//
+//  Created by Tautvydas Stakėnas on 2/21/15.
+//  Copyright (c) 2015 Tautvydas Stakėnas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FactController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Facts.swift
@@ -0,0 +1,14 @@
+//
+//  Facts.swift
+//  Blower
+//
+//  Created by Tautvydas Stakėnas on 2/21/15.
+//  Copyright (c) 2015 Tautvydas Stakėnas. All rights reserved.
+//
+
+import Foundation
+
+
+class Facts {
+    
+}
